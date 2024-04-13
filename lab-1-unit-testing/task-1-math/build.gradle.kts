772c4c30d03d8f0c0da420a94081d1d42238af13
e1turin <ivturnik2003@gmail.com>
2024-04-13 17:18:17 +0300

    init

---
+++ lab-1-unit-testing/task-1-math/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+}
+
+group = "io.github.e1turin"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
