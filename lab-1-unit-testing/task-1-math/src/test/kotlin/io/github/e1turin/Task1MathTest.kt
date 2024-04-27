4f109a350d5f1d33573bd7750b8cee82baf5b299
e1turin <ivturnik2003@gmail.com>
2024-04-27 22:22:57 +0300

    basic property tests for arcsin

--- lab-1-unit-testing/task-1-math/src/test/kotlin/io/github/e1turin/Task1MathTest.kt
+++ lab-1-unit-testing/task-1-math/src/test/kotlin/io/github/e1turin/Task1MathTest.kt
@@ -1,15 +1,41 @@
 package io.github.e1turin
 
 import io.github.e1turin.math.arcsin
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.ranges.shouldBeIn
+import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.double
+import io.kotest.property.checkAll
+import kotlin.math.abs
 import kotlin.math.asin
 import kotlin.test.Test
 
-class Task1MathTest {
+internal class Task1MathKotlinTest {
 
     @Test
-    fun `golden test arcsin`() {
+    fun `golden test asin`() {
         val x = 0.0
         assert(arcsin(x) == asin(x))
     }
+
 }
 
+internal class Task1MathKotest : AnnotationSpec() {
+    @Test
+    suspend fun `golden test asin`() {
+        Arb.double(min = -1.0, max = 1.0).checkAll { d ->
+            val value = arcsin(d)
+            abs((value - asin(d)) / value) shouldBeIn (0.0..0.35)
+        }
+    }
+}
+
+internal class Task2MathPropertyTest : StringSpec({
+
+    "kek" {
+        "hello".length shouldBe 5
+    }
+
+})
