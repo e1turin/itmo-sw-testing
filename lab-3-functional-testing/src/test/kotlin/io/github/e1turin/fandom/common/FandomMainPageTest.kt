5589d4c3b76b68cda800a287905c2cff9e7fd6fd
e1turin <ivturnik2003@gmail.com>
2024-09-25 01:36:51 +0300

    Add scenario to go to main from another page & enhancement

--- lab-3-functional-testing/src/test/kotlin/io/github/e1turin/fandom/common/FandomMainPageTest.kt
+++ lab-3-functional-testing/src/test/kotlin/io/github/e1turin/fandom/common/FandomMainPageTest.kt
@@ -9,14 +9,23 @@ import org.openqa.selenium.WebElement
 
 fun buildTestSuite(driverSetup: WebDriver) = stringSpec {
     lateinit var driver: WebDriver
+    lateinit var someFandomPage: FandomPage
+
+    val randomSiteURL = "https://matrix.fandom.com/wiki/Neo"
 
     beforeTest {
         driver = driverSetup
+
+        someFandomPage = object : FandomPage(driver, randomSiteURL) {
+            override fun status(): PageStatus = onThisPageIf(title().contains("Neo"))
+        }
     }
 
     afterTest {
         driver.quit()
     }
+
+    fun By.find(): WebElement = driver.findElement(this)
 
     fun onMainPage(steps: FandomMainPage.() -> Unit) {
         val mainPage = FandomMainPage(driver)
@@ -26,7 +35,13 @@ fun buildTestSuite(driverSetup: WebDriver) = stringSpec {
         }
     }
 
-    fun By.find(): WebElement = driver.findElement(this)
+    fun onSomePage(steps: FandomPage.() -> Unit) {
+        with(someFandomPage) {
+            open()
+            someFandomPage.status() shouldBe CurrentPage
+            steps()
+        }
+    }
 
     "Open main page" {
         onMainPage {
@@ -34,11 +49,18 @@ fun buildTestSuite(driverSetup: WebDriver) = stringSpec {
         }
     }
 
-    "Go to main page and remain here" {
+    "Open main page and remain here after recursive navigation" {
         onMainPage {
             goToHome()
             status() shouldBe CurrentPage
         }
+    }
+
+    "Go to main page from random page" {
+        onSomePage {
+            goToHome()
+        }
+        FandomMainPage(driver).status() shouldBe CurrentPage
     }
 
     "Open search bar and start searching" {
